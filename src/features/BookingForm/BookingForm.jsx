e2f7624275941d5b25e050bf5c8a00ae4b4a3cd5
Tushar Pramanik <tusharpramanikbd@gmail.com>
2025-06-14 13:37:59 +0600

    added form state

--- src/features/BookingForm/BookingForm.jsx
+++ src/features/BookingForm/BookingForm.jsx
@@ -1,24 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import "./BookingForm.css";
 
+const availableTimes = ["17:00", "18:00", "19:00", "20:00", "21:00", "22:00"];
+
 function BookingForm() {
+  const [date, setDate] = useState("");
+  const [bookingTime, setBookingTime] = useState(availableTimes[0]);
+  const [numOfGuests, setNumOfGuests] = useState("1");
+  const [occasion, setOccation] = useState("Birthday");
+
   return (
     <form className="booking-form-container">
       <label htmlFor="res-date">Choose date</label>
-      <input type="date" id="res-date" />
+      <input
+        type="date"
+        id="res-date"
+        value={date}
+        onChange={(e) => setDate(e.target.value)}
+      />
       <label htmlFor="res-time">Choose time</label>
-      <select id="res-time">
-        <option>17:00</option>
-        <option>18:00</option>
-        <option>19:00</option>
-        <option>20:00</option>
-        <option>21:00</option>
-        <option>22:00</option>
+      <select
+        id="res-time"
+        value={bookingTime}
+        onChange={(e) => setBookingTime(e.target.value)}
+      >
+        {availableTimes.map((val, index) => (
+          <option key={index}>{val}</option>
+        ))}
       </select>
       <label htmlFor="guests">Number of guests</label>
-      <input type="number" placeholder="1" min="1" max="10" id="guests"></input>
+      <input
+        type="number"
+        placeholder="1"
+        min="1"
+        max="10"
+        id="guests"
+        value={numOfGuests}
+        onChange={(e) => setNumOfGuests(e.target.value)}
+      />
       <label htmlFor="occasion">Occasion</label>
-      <select id="occasion">
+      <select
+        id="occasion"
+        value={occasion}
+        onChange={(e) => setOccation(e.target.value)}
+      >
         <option>Birthday</option>
         <option>Anniversary</option>
       </select>
